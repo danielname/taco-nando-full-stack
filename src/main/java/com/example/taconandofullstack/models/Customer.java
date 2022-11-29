747da9ba7514a58819e613dc3123c59c3a05e967
Daniel Name <daniel.v.name@gmail.com>
2022-11-29 15:56:24 -0600

    added partials file and cleaned up customer class

--- src/main/java/com/example/taconandofullstack/models/Customer.java
+++ src/main/java/com/example/taconandofullstack/models/Customer.java
@@ -1,9 +1,13 @@
 package com.example.taconandofullstack.models;
 
+import java.util.List;
+
 public class Customer {
     private int id;
     private String username;
     private String password;
+    private String email;
     private String first_name;
     private String last_name;
+    private List<Menu> favorites;
 }

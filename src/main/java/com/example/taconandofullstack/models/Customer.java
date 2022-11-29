8a524fed3aa4e550676ecb01ee485fbc9c775360
Daniel Name <daniel.v.name@gmail.com>
2022-11-29 15:21:39 -0600

    filled out the customer class and created the menu class

--- src/main/java/com/example/taconandofullstack/models/Customer.java
+++ src/main/java/com/example/taconandofullstack/models/Customer.java
@@ -1,7 +1,7 @@
 package com.example.taconandofullstack.models;
 
 public class Customer {
-    private long id;
+    private int id;
     private String username;
     private String password;
     private String first_name;
---
+++ src/main/java/com/example/taconandofullstack/models/menu.java
@@ -0,0 +1,9 @@
+package com.example.taconandofullstack.models;
+
+public class menu {
+    private short id;
+    private String item;
+    private short quantityInStock;
+    private float price;
+    private boolean special;
+}
